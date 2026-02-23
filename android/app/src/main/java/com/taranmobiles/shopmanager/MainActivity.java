3db81f355efeb64c12ce5e9d37118a74ab9f10e8
Devanshu Pruthi <devanshu32165.com>
2026-02-24 00:11:59 +0530

    Revert "apk"
    This reverts commit 7bfb90cd47e47be6ed8e2adbc2ac063746b76d98.

--- android/app/src/main/java/com/taranmobiles/shopmanager/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.taranmobiles.shopmanager;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.taranmobiles.shopmanager',
-  appName: 'Shop Manager',
-  webDir: 'dist/demo/browser'
-};
-
-export default config;
