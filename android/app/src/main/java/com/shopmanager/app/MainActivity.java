3ae2272739efccb232c9d4457d10b9bef7ee2a6e
Devanshu Pruthi <devanshu32165.com>
2026-02-24 00:26:17 +0530

    apk config

---
+++ android/app/src/main/java/com/shopmanager/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shopmanager.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.shopmanager.app',
+    appName: 'Shop Manager',
+    webDir: 'dist/demo/browser',
+    bundledWebRuntime: false
+};
+
+export default config;
