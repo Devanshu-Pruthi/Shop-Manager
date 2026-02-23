ce1d49ce59088b4995059a66efd9ffb972826b39
Devanshu Pruthi <devanshu32165.com>
2026-02-24 00:29:47 +0530

    fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'com.shopmanager.app',
     appName: 'Shop Manager',
-    webDir: 'dist/demo/browser',
-    bundledWebRuntime: false
+    webDir: 'dist/demo/browser'
 };
 
 export default config;
