7bfb90cd47e47be6ed8e2adbc2ac063746b76d98
Devanshu Pruthi <devanshu32165.com>
2026-02-24 00:06:02 +0530

    apk

---
+++ android/app/src/main/java/com/taranmobiles/shopmanager/MainActivity.java
@@ -0,0 +1,5 @@
+package com.taranmobiles.shopmanager;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taranmobiles.shopmanager',
+  appName: 'Shop Manager',
+  webDir: 'dist/demo/browser'
+};
+
+export default config;
